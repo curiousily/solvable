cf874e268dec5ed4fc712f4c09c505fc81c6039a
Venelin Valkov <venelin@masconsult.eu>
2011-08-28 01:47:27 +0300

    Added macro for NonNegative check

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "tester.hpp"
+
+using namespace std;
+
+int main()
+{
+//    cout << "Hello world!" << endl;
+//    NotNegative(-1, __FUNCTION__, __LINE__);
+    NNegative(-1);
+    WriteSuccess();
+    return 0;
+}
